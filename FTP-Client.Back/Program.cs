799532ab96681862ec097b64e19b6b09f241be4c
Jordan Gaslonde <J.Gaslonde@outlook.com>
2019-07-10 20:00:48 +0100

    Add project files.

---
+++ FTP-Client.Back/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FTP_Client.Back
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
